ddf1f8855816848f6a8f40c1b4e2319984a5582d
htmlbear <alex.ulbashev@gmail.com>
2016-10-28 21:56:32 +0300

    safety p1

--- wp-content/themes/wp-starter/functions.php
+++ wp-content/themes/wp-starter/functions.php
@@ -103,3 +103,22 @@ function my_scripts_method() {
 }    
  
 add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
+
+
+//  remove default widgets wp 
+
+function remove_dashboard_widgets() {
+	global $wp_meta_boxes;
+
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_drafts']);
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+
+}
+
+add_action('wp_dashboard_setup', 'remove_dashboard_widgets' );
